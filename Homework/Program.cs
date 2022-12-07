1db47c9f07ad07505466191b9e234c0a925d2961
Shamil <shoma994@gmail.com>
2022-12-07 17:06:37 +0300

    homework_2

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,4 +1,4 @@
-﻿//Домашняя работа №1
+﻿                                        //Домашняя работа №1
 
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // Console.Write("Input first number: ");
@@ -62,3 +62,74 @@
 //     }
 //     current ++;
 // }
+
+                                           
+                                           
+                                           
+                                            
+                                            // Домашняя работа № 2
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int CutNumber(int num)
+// {
+//     int ed = num % 100;
+//     int result = ed / 10;
+//     return result;
+// }
+
+// Console.Write("Введите трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int newNum = CutNumber(num);
+
+// if(num > 99 && num < 1000)
+// Console.WriteLine($"New version of number {num} is {newNum}");
+// else
+// {  
+//     Console.Write("Данное число не трехзначное! Повторите попытку, перезапустив прогамму.");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет (-1).
+
+//  int Third(int number)
+// {
+//     int result = -1;
+//     if (number >= 100)
+//     {
+//         while (number > 999)
+//         {
+//             number = number / 10;
+//         }
+//         result = number % 10;
+//     }
+//     return result;
+             
+// }
+
+// Console.Write("Input an integer number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int newnum =Third(num);
+// Console.Write($"Its an integer number - {num} and third element this number -  {newnum}");
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.WriteLine("Введите номер дня недели");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// void dayoff(int number)
+// {
+//         if(number >= 1 && number <= 5)
+//     {
+//         Console.WriteLine("Это рабочий день недели");  
+//     }
+//     else if(number >=6 && number <= 7)
+//     {
+//          Console.WriteLine("Этот день недели выходной");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Такого дня недели не существует");
+//     }
+// }
+// dayoff(number);
